bc8f3df3d0e0f827f6fb513a5c6e4d56af5136e7
nico <nicollosa35@gmail.com>
2023-05-15 08:36:16 +0800

    Seonc added some functions

--- src/MainPanel.java
+++ src/MainPanel.java
@@ -5,8 +5,7 @@ public class MainPanel extends Runnable {
     static Runnable runnable = new Runnable();
     private JLabel jcomp1;
     private JButton makeReservationBtn;
-    private JButton jcomp3;
-    private JButton jcomp4;
+    private JButton bookingBtn;
     private JButton orderFoods;
 
 
@@ -14,8 +13,8 @@ public class MainPanel extends Runnable {
     public MainPanel(){
         jcomp1 = new JLabel ("OnlyFoods");
         makeReservationBtn = new JButton ("Make Reservation");
-        jcomp3 = new JButton ("View Bookings");
-        jcomp4 = new JButton ("Cancel Reservations");
+        bookingBtn = new JButton ("View Bookings");
+
         orderFoods = new JButton ("Order Food");
 
         //adjust size and set layout
@@ -25,16 +24,16 @@ public class MainPanel extends Runnable {
         //add components
         add (jcomp1);
         add (makeReservationBtn);
-        add (jcomp3);
-        add (jcomp4);
+        add (bookingBtn);
+
         add (orderFoods);
 
         //set component bounds (only needed by Absolute Positioning)
         jcomp1.setBounds (335, 55, 100, 25);
         makeReservationBtn.setBounds (235, 110, 270, 55);
-        jcomp3.setBounds (235, 175, 270, 55);
-        jcomp4.setBounds (235, 245, 270, 50);
-        orderFoods.setBounds (235, 315, 270, 55);
+        bookingBtn.setBounds (235, 175, 270, 55);
+
+        orderFoods.setBounds (235, 245, 270, 55);
 
         makeReservationBtn.addActionListener(isClicked -> {
             Reservation reservationPanel = new Reservation();
